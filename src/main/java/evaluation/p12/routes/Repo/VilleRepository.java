55ca65dbcb00b69c69d36fdc8e7c9667747fa52a
andrianzo23 <80960318+andrianzo23@users.noreply.github.com>
2021-06-21 13:42:58 +0200

    Create VilleRepository.java

---
+++ src/main/java/evaluation/p12/routes/Repo/VilleRepository.java
@@ -0,0 +1,9 @@
+package evaluation.p12.routes.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import evaluation.p12.routes.models.Ville;
+
+public interface VilleRepository extends JpaRepository<Ville,Integer> {
+
+}
